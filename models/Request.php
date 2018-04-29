feb64914fe374eebbaf2344fe3fecb91037df753
vleclerc <vleclerc.dev@gmail.com>
2018-04-29 17:15:09 +0200

    test

--- models/Request.php
+++ models/Request.php
@@ -28,8 +28,6 @@ class Request {
         }
         
         $data = json_decode(file_get_contents('php://input'), true);
-        var_dump($data);
-        
         $this->parameter = $data;
     }
 }
