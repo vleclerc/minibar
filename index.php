567ff6cfdcd785c55656af2f2c30a0872dba9c02
vleclerc <vleclerc.dev@gmail.com>
2018-04-29 16:49:16 +0200

    test

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
 require_once 'autoloader.php';
-    
+
+var_dump($_SERVER);
+var_dump($_POST);
+var_dump($_GET);
+var_dump($_REQUEST);
+
+
 $request = new Request();
 
 $gpioController = new GpioController();
