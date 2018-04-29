8e88f7fedad385a89f2869d1ba15b0d16898d68e
vleclerc <vleclerc.dev@gmail.com>
2018-04-29 16:44:07 +0200

    dump

--- autoloader.php
+++ autoloader.php
@@ -3,19 +3,18 @@ spl_autoload_register(function ($class_name) {
     
     var_dump($class_name);
     
-    var_dump(dirname(__FILE__));
     
     if (file_exists($className . '.php')) {
         var_dump('index');
         require_once $class_name . '.php';
     }
-    else if (file_exists('models/' . $className . '.php')) {
+    else if (file_exists(dirname(__FILE__) . '/models/' . $className . '.php')) {
         var_dump('models');
-        require_once 'models/' . $class_name . '.php';
+        require_once dirname(__FILE__) . '/models/' . $class_name . '.php';
     }
-    else if (file_exists('controllers/' . $className . '.php')) {
+    else if (file_exists(dirname(__FILE__) . '/controllers/' . $className . '.php')) {
         var_dump('controllers');
-        require_once 'controllers/' . $class_name . '.php';
+        require_once dirname(__FILE__) . '/controllers/' . $class_name . '.php';
     } else {
         var_dump('not found');
     }
