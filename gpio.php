c0f4c7b2abd8b8d2aad2421a1a2286471a6a2a70
vleclerc <vleclerc.dev@gmail.com>
2018-04-29 16:27:51 +0200

    extract autoloader

--- gpio.php
+++ gpio.php
@@ -1,17 +1,6 @@
 <?php
 
-class Request {
-    public $parameter;
-    public $method;
-    
-    public function __construct(){
-        $this->method = $_SERVER['REQUEST_METHOD'];
-        
-        $data = json_decode(file_get_contents('php://input'), true);
-        
-        $this->parameter = $data;
-    }
-}
+require_once 'autoloader.php';
 
 $request = new Request();
 
--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php
 
-spl_autoload_register(function ($class_name) {
-    
-    var_dump($class_name);
-    
-    if (file_exists($className . '.php')) { 
-        require_once $class_name . '.php';
-    }
-    if (file_exists('models/' . $className . '.php')) {
-        require_once 'models/' . $class_name . '.php';
-    }
-    if (file_exists('controllers/' . $className . '.php')) {
-        require_once 'controllers/' . $class_name . '.php';
-    }
-    
-});
+require_once 'autoloader.php';
     
 $request = new Request();
 
