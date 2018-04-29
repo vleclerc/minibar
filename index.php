b4f51d7a629ddc713a038cccd5a709f63b2607e8
vleclerc <vleclerc.dev@gmail.com>
2018-04-29 16:20:58 +0200

    autoloader

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+spl_autoload_register(function ($class_name) {
+    if (file_exists($className . '.php')) { 
+        require_once $class_name . '.php';
+    }
+    if (file_exists('models/' . $className . '.php')) {
+        require_once 'models/' . $class_name . '.php';
+    }
+    if (file_exists('controllers/' . $className . '.php')) {
+        require_once 'controllers/' . $class_name . '.php';
+    }
+    
+});
+    
+
 class Request {
     public $parameter;
     public $method;
