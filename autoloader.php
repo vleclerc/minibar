8ac4d44a61ce8eaa035abee6f8525f9bf4a7ab94
vleclerc <vleclerc.dev@gmail.com>
2018-04-29 16:47:00 +0200

    fix autoloader

--- autoloader.php
+++ autoloader.php
@@ -1,23 +1,16 @@
 <?php
-spl_autoload_register(function ($class_name) {
-    
-    var_dump(dirname(__FILE__) . '/models/' . $className . '.php');
-    var_dump(file_exists(dirname(__FILE__) . '/models/' . $className . '.php'));
-    
+spl_autoload_register(function ($className) {
     
     if (file_exists($className . '.php')) {
-        var_dump('index');
-        require_once $class_name . '.php';
+        require_once $className . '.php';
     }
     else if (file_exists(dirname(__FILE__) . '/models/' . $className . '.php')) {
-        var_dump('models');
-        require_once dirname(__FILE__) . '/models/' . $class_name . '.php';
+        require_once dirname(__FILE__) . '/models/' . $className . '.php';
     }
     else if (file_exists(dirname(__FILE__) . '/controllers/' . $className . '.php')) {
-        var_dump('controllers');
-        require_once dirname(__FILE__) . '/controllers/' . $class_name . '.php';
+        require_once dirname(__FILE__) . '/controllers/' . $className . '.php';
     } else {
-        var_dump('not found');
+        var_dump($className.' not found');
     }
     
 });
