f4c2cee4e7a158af9a44713ac05f78a7f41fc2de
vleclerc <vleclerc.dev@gmail.com>
2018-04-29 16:33:37 +0200

    dump

--- models/Request.php
+++ models/Request.php
@@ -6,7 +6,12 @@ class Request {
     public function __construct(){
         $this->method = $_SERVER['REQUEST_METHOD'];
         
+        
+        var_dump($this->method);
+        
         $data = json_decode(file_get_contents('php://input'), true);
+        
+        var_dump($data);
         
         $this->parameter = $data;
     }
